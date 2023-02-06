28192036edfab7728fd9dbc73f6e6fb86852ebd5
okoth kongo <okothkongo@gmail.com>
2023-02-06 13:08:18 +0300

    setup services

--- deps_setup/editors.sh
+++ deps_setup/editors.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 function setup_editors(){
+  sudo apt-get -y install software-properties-common apt-transport-https wget
+  sudo apt-get  -y
   wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
-  sudo apt install code -y
+  sudo apt-get install code -y
   sudo apt-get install neovim -y
   
 }
