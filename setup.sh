92cd07248fd62ac7c5de22c1144c4181ad157aaf
okoth kongo <okothkongo@gmail.com>
2023-02-06 11:48:20 +0300

    setup editors

--- setup.sh
+++ setup.sh
@@ -12,12 +12,14 @@ function initial_setup (){
   sudo apt install software-properties-common apt-transport-https wget -y
   source deps_setup/git_and_asdf.sh
   source deps_setup/shell.sh
+  source deps_setup/editors.sh
 }
 if [[ $MYOS == "Linux" ]] ; then
   echo "This installation setup for Linux fitting $(lsb_release -d) has began ..."
   initial_setup
   setup_shell
   setup_git_and_asdf
+  setup_editors
   #TO DO implement source for zshrc
   source ~/.bashrc
  echo "Installations Successful."
