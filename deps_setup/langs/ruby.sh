8a5d9c6e726f128bdcd1eb69ef86a7648735dfa1
okoth kongo <34419890+okothkongo@users.noreply.github.com>
2023-02-11 13:48:05 +0300

    Update ruby.sh

--- deps_setup/langs/ruby.sh
+++ deps_setup/langs/ruby.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 function setup_ruby(){
   asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
-  wget http://pyyaml.org/download/libyaml/yaml-0.2.5.tar.gz
   asdf install ruby 3.1.2
   asdf global ruby 3.1.2
   gem install bundler rails
